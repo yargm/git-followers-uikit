8b84e3a61feb3b7deff50012a170c5276b8eb0e2
Yareni Mérida <yargm2304@gmail.com>
2024-10-14 16:50:56 -0600

    Second commit

--- GITAPP/Utilities/UIHelper.swift
+++ GITAPP/Utilities/UIHelper.swift
@@ -10,7 +10,7 @@ import UIKit
 struct UIHelper {
     static  func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
         let width = view.bounds.width
-        let padding: CGFloat = 12
+        let padding: CGFloat = 11
         let minimumItemSpacing: CGFloat = 10
         let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
         let itemWidth = availableWidth / 3
